d4fa0a901837fc50615e0122b700d779bd75c21e
jinhoyang <dkaah613@naver.com>
2021-01-25 21:29:10 +0900

    enter

---
+++ index.js
@@ -0,0 +1,7 @@
+function sayHello(name, age){
+    console.log('Hello!', name, "you have", age, "years of age");
+}
+
+sayHello("nicolas", 15);
+//argument는무한대로 가질수 있다
+//지금 name- argument 값을 nicolas 가 받아서 출력을 한다
